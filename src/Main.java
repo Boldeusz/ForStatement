561282b2cd80d9478629df34658b5d42c1cc5d3f
Boldeusz <mnpower@poczta.onet.pl>
2021-01-20 15:12:54 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,46 @@ public class Main {
 //            System.out.println("Loop " + i + " hello!");
 //        }
 
-        for (int i=2; i<9 ; i++ ){
-            System.out.println("10,000 at " + i + " % interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+//        for (int i=2; i<9 ; i++ ){
+//            System.out.println("10,000 at " + i + " % interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+//        }
+
+//        System.out.println(isPrime(12));
+
+//        for (int i=8; i>1 ; i-- ){
+//            System.out.println("10,000 at " + i + " % interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+//        }
+
+        int count = 0;
+        for (int i = 1; i < 100; i++) {
+            if (isPrime(i)) {
+                count++;
+                System.out.println("Number " + i + " is a prime number");
+                if (count == 3) {
+                    break;
+                }
+            }
+
         }
 
 
     }
-    public static double calculateInterest(double amount, double interestRate){
-        return (amount * (interestRate/100));
+
+    public static boolean isPrime(int n) {
+
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
     }
 }
