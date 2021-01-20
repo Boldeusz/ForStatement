5f6cf0b3fb6cde9d440139d34b47300fd63c1b59
Boldeusz <mnpower@poczta.onet.pl>
2021-01-20 15:22:37 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,8 @@ public class Main {
             if (isPrime(i)) {
                 count++;
                 System.out.println("Number " + i + " is a prime number");
-                if (count == 3) {
+                if (count == 13) {
+                    System.out.println("Exiting for loop");
                     break;
                 }
             }
@@ -39,8 +40,10 @@ public class Main {
         if (n == 1) {
             return false;
         }
-        for (int i = 2; i <= n / 2; i++) {
-//            for (int i = 2; (long) Math.sqrt(n); i++) {      second solution
+//        for (int i = 2; i <= n / 2; i++) {
+        for (int i = 2; i <= (long) Math.sqrt(n); i++) {
+            System.out.println("Looping " +i);
+
             if (n % i == 0) {
                 return false;
             }
